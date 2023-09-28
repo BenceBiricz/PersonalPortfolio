47ca3d8c010964a96f2c5bf084a197b0990aaa2c
BenceBiricz <biriczbence@gmail.com>
2023-09-28 14:21:50 +0200

    Relative Display based on resolution.

--- PersonalPortfolioProject/Controllers/HomeController.cs
+++ PersonalPortfolioProject/Controllers/HomeController.cs
@@ -7,6 +7,73 @@ namespace PersonalPortfolioProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static List<PortfolioViewModel> _ports = new List<PortfolioViewModel>()
+        {
+            new PortfolioViewModel()
+            {
+                Id = 1,
+                CardHeader = "VR",
+                CardTitle  = "Motion Tracking",
+                ImgPath = "Images\\vr.png",
+                CardText = "Godot plugin to track the users motion and measure VR Jitter.",
+                GitHubLink = "https://github.com/BenceBiricz/VR-movement-tracking",
+            },
+            new PortfolioViewModel()
+            {
+                Id = 2,
+                CardHeader = "VR",
+                CardTitle  = "Eye/Gaze Tracking",
+                ImgPath = "Images\\eye.jpg",
+                CardText = "Eye tracking and gaze tracking in virtual space. Images are in a python app.",
+                GitHubLink = "https://github.com/BenceBiricz/VR_Eye_Gaze_Tracking",
+            },
+            new PortfolioViewModel()
+            {
+                Id = 3,
+                CardHeader = "ASP .NET",
+                CardTitle  = "Smart advisor",
+                ImgPath = "Images\\phone.png",
+                CardText = "Basic WEB application for impementing OpenAi API to a webpage.",
+                GitHubLink = "https://github.com/BenceBiricz/SmartPhoneTechAdvisor",
+            },
+            new PortfolioViewModel()
+            {
+                Id = 4,
+                CardHeader = "Unity",
+                CardTitle  = "3D RPG Game Dev",
+                ImgPath = "Images\\unity.png",
+                CardText = "A 3D RPG game created in Unity called Hero's Journey.",
+                GitHubLink = "https://github.com/BenceBiricz/3D_game_Unity",
+            },
+            new PortfolioViewModel()
+            {
+                Id = 5,
+                CardHeader = "Python",
+                CardTitle  = "Image Processing",
+                ImgPath = "Images\\segmentation.png",
+                CardText = "Segmentation of surgical devices on Synthetic MICCAI dataset.",
+                GitHubLink = "https://github.com/BenceBiricz/Medical_Instrument_Segmentation",
+            },
+            new PortfolioViewModel()
+            {
+                Id = 6,
+                CardHeader = "C#",
+                CardTitle  = "Edge Detection",
+                ImgPath = "Images\\sobel.jpg",
+                CardText = "Sobel Edge detection algorithm created in C# with threading.",
+                GitHubLink = "https://github.com/BenceBiricz/Sobel_Edge_Detection",
+            },
+            new PortfolioViewModel()
+            {
+                Id = 7,
+                CardHeader = "C#",
+                CardTitle  = "Emotion Detection",
+                ImgPath = "Images\\emotion.png",
+                CardText = "The task is to detect and recognise the emotions of the human face.",
+                GitHubLink = "https://github.com/BenceBiricz/Emotion_Detection",
+            }
+
+        };
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,7 +82,7 @@ namespace PersonalPortfolioProject.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(model:_ports);
         }
 
         public IActionResult Privacy()
---
+++ PersonalPortfolioProject/Models/PortfolioViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalPortfolioProject.Models
+{
+    public class PortfolioViewModel
+    {
+        public int Id { get; set; }
+        public string CardHeader { get; set; }
+        public string CardTitle { get; set; }
+        public string ImgPath { get; set; }
+        public string CardText { get; set; }
+        public string GitHubLink { get; set; }
+    }
+}
